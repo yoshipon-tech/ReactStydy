d31d83f6dc9a920a4d7a126260d9a7a807cd9780
yoshipon-tech <yoshipon.tech@gmail.com>
2024-02-14 22:49:45 +0900

    フォーム入力欄の実施

--- src/compornents/AddTodo.tsx
+++ src/compornents/AddTodo.tsx
@@ -1,11 +1,25 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 // 関数型のコンポーネント定義
 const AddTodo: FC = () => {
+  //onclick時のロジックをここに記入する
+  const [todo, setTodo] = useState("");
+
+  const handleTodoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo(event.target.value);
+    console.log("fuga");
+  };
+
+  // 関数としてaddTodoの定義が必要
+  const addTodo = () => {
+    console.log("hoge");
+  };
+
   return (
-    <div>
-      Todoリスト追加用のコンポーネント
-    </div>
+    <>
+      <input type="text" value={todo} onChange={handleTodoChange}></input>
+      <button onClick={addTodo}>送信</button>
+    </>
   );
 };
 
